dbbaa030a655d55dcf0eca358553bc95eb4010b0
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2017-07-02 15:23:09 +0800

    Re-configure cluster nodes
    - 8GB droplet with 20GB external storage
    - remove worker nodes

--- terraform/master.tf
+++ terraform/master.tf
@@ -1,14 +1,36 @@
-resource "digitalocean_droplet" "master-1" {
+resource "digitalocean_volume" "master-volume-1" {
+  region = "sfo2"
+  name = "master-volume-1"
+  size = "20"
+}
+
+resource "digitalocean_droplet" "master" {
   image = "${var.do_image_id}"
-  name = "master-1"
+  name = "master"
   region = "sfo2"
   size = "8gb"
   ssh_keys = ["${var.do_ssh_key}"]
+  user_data ="${file("user_data/openshift.yml")}"
+  volume_ids = ["${digitalocean_volume.master-volume-1.id}"]
 }
 
-resource "digitalocean_record" "master-1" {
+resource "digitalocean_record" "master" {
   domain = "${var.do_domain}"
   type = "A"
-  name = "master-1"
-  value = "${digitalocean_droplet.master-1.ipv4_address}"
+  name = "master"
+  value = "${digitalocean_droplet.master.ipv4_address}"
+}
+
+resource "digitalocean_record" "wildcard" {
+  domain = "${var.do_domain}"
+  type = "A"
+  name = "*"
+  value = "${digitalocean_droplet.master.ipv4_address}"
+}
+
+resource "digitalocean_record" "apex" {
+  domain = "${var.do_domain}"
+  type = "A"
+  name = "@"
+  value = "${digitalocean_droplet.master.ipv4_address}"
 }
